153414f3ba33c98510847baeb429c3678be7203c
amishra9919 <86140544+amishra9919@users.noreply.github.com>
2024-05-05 12:59:13 +0530

    restore paragraph issue

--- 05_Testing_code/input_link.py
+++ 05_Testing_code/input_link.py
@@ -1,7 +1,4 @@
 import joblib
-
-
-
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -11,9 +8,9 @@ def preprocess_text(text):
     text = text.lower()
     
     text = " ".join(text.split())                                                #remove spaces
-    
+
     text = re.sub(r'[^a-zA-Z0-9\s.]', '', text)                                  #remove special symbols and characters
-    
+
     tokens = text.split()                                                        #tokenize text by space
     
     stop_words = set(stopwords.words('english'))                                 #remove stopwords except full stop
@@ -21,25 +18,26 @@ def preprocess_text(text):
 
     filtered_tokens = [token for token in tokens if token not in stop_words]     #remove stopwords
 
-    # Join tokens back into text
     preprocessed_text = " ".join(filtered_tokens)
-    
+    # print("preprocessed_text inside preproceeess_text funciton: ", preprocessed_text)
     return preprocessed_text
 
 def extract_paragraphs_from_link(url):
 
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
 
         paragraphs = soup.find_all('p')
-
+        # print("parass: ", paragraphs)
         if len(paragraphs) != 0:
+            text = ''
             for paragraph in paragraphs:          #extracting the text form each para 
                 # print("inside if ")
-                text = paragraph.get_text()
-                preprocessed_text = preprocess_text(text)
+                text += paragraph.get_text() 
+            preprocessed_text = preprocess_text(text) 
+            # print("preprocessed_text: ", preprocessed_text)
         else:                                 #Failed to fetch the content"
             preprocessed_text = ''
 
@@ -48,17 +46,20 @@ def extract_paragraphs_from_link(url):
         preprocessed_text = ''
     return preprocessed_text
 
+if __name__ == "__main__":
 
-news_link = "https://indianexpress.com/article/sports/cricket/dc-vs-gt-live-score-ipl-2024-match-40-today-delhi-capitals-vs-gujarat-titans-scorecard-updates-9287629/"
-paragraph = extract_paragraphs_from_link(news_link)
+    news_link = "https://timesofindia.indiatimes.com/city/kolkata/didi-dares-pm-narendra-modi-to-debate-her-on-basics-of-hinduism/articleshow/109851621.cms"
+    paragraph = extract_paragraphs_from_link(news_link)
+    print('paragraph: ', paragraph)
 
-if len(paragraph) == 0:
-    print("Unable to fetch the content")
-else:
-    # print("inside else in model loading")
-    #Loding model
-    model = joblib.load(r'D:\VCET\BE_Project\News_Text_Classification\04_Trained_models\TFIDF_SVM.pkl')
+    if len(paragraph) == 0:
+        print("Unable to fetch the content")
+    else:
+        # print("Paragraph: ", paragraph)
+        print("inside else in model loading")
+        #Loding model
+        model = joblib.load(r'C:\Users\DELL\OneDrive\Desktop\major project\News-Text-Classification org\04_Trained_models\TFIDF_SVM.pkl')
 
-    new_text = [paragraph]
-    predictions = model.predict(new_text)
-    print("Predicted categories for the input text:", predictions)
+        new_text = [paragraph]
+        predictions = model.predict(new_text)
+        print("Predicted categories for the input text:", predictions)
