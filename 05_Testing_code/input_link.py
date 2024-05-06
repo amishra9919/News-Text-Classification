9f3d73a3f4a1c792914f30373cfee530e8ef628e
amishra9919 <86140544+amishra9919@users.noreply.github.com>
2024-05-06 17:45:16 +0530

    solved model path issue

--- 05_Testing_code/input_link.py
+++ 05_Testing_code/input_link.py
@@ -1,3 +1,4 @@
+import os
 import joblib
 import requests
 from bs4 import BeautifulSoup
@@ -6,60 +7,50 @@ from nltk.corpus import stopwords
 
 def preprocess_text(text):
     text = text.lower()
+    text = " ".join(text.split())
+    text = re.sub(r'[^a-zA-Z0-9\s.]', '', text)
+    tokens = text.split()
     
-    text = " ".join(text.split())                                                #remove spaces
-
-    text = re.sub(r'[^a-zA-Z0-9\s.]', '', text)                                  #remove special symbols and characters
-
-    tokens = text.split()                                                        #tokenize text by space
-    
-    stop_words = set(stopwords.words('english'))                                 #remove stopwords except full stop
+    stop_words = set(stopwords.words('english'))
     stop_words.discard('.')
-
-    filtered_tokens = [token for token in tokens if token not in stop_words]     #remove stopwords
-
+    filtered_tokens = [token for token in tokens if token not in stop_words]
     preprocessed_text = " ".join(filtered_tokens)
-    # print("preprocessed_text inside preproceeess_text funciton: ", preprocessed_text)
+    
     return preprocessed_text
 
 def extract_paragraphs_from_link(url):
-
     response = requests.get(url)
-
+    
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-
         paragraphs = soup.find_all('p')
-        # print("parass: ", paragraphs)
+        
         if len(paragraphs) != 0:
-            text = ''
-            for paragraph in paragraphs:          #extracting the text form each para 
-                # print("inside if ")
-                text += paragraph.get_text() 
-            preprocessed_text = preprocess_text(text) 
-            # print("preprocessed_text: ", preprocessed_text)
-        else:                                 #Failed to fetch the content"
             preprocessed_text = ''
-
+            for paragraph in paragraphs:
+                text = paragraph.get_text()
+                preprocessed_text += preprocess_text(text) + ' '
+        else:
+            preprocessed_text = ''
     else:
         print("Failed to fetch the content")
         preprocessed_text = ''
+    
     return preprocessed_text
 
-if __name__ == "__main__":
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(current_dir, '..', '04_Trained_models', 'TFIDF_SVM.pkl')
 
-    news_link = "https://www.bbc.com/sport/cricket/articles/c1d4jyk300do"
-    paragraph = extract_paragraphs_from_link(news_link)
-    print('paragraph: ', paragraph)
+news_link = "https://www.bbc.com/sport/cricket/articles/c1d4jyk300do"
+paragraph = extract_paragraphs_from_link(news_link)
 
-    if len(paragraph) == 0:
-        print("Unable to fetch the content")
-    else:
-        # print("Paragraph: ", paragraph)
-        print("inside else in model loading")
-        #Loding model
-        model = joblib.load(r'C:\Users\DELL\OneDrive\Desktop\major project\News-Text-Classification org\04_Trained_models\TFIDF_SVM.pkl')
+if len(paragraph) == 0:
+    print("Unable to fetch the content")
+else:
+    # print(paragraph)
+    #loading model
+    model = joblib.load(model_path) 
 
-        new_text = [paragraph]
-        predictions = model.predict(new_text)
-        print("Predicted categories for the input text:", predictions)
+    new_text = [paragraph]
+    predictions = model.predict(new_text)
+    print("Predicted categories for the input text: ", predictions[0])
